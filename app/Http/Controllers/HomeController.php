e24ebfbb9cf198cf3ab56fe9962e8c641b9f264e
Robson Fernando Trasel de Souza <95061628+RobsonTrasel@users.noreply.github.com>
2022-07-20 23:42:46 -0300

    setup web route & homecontroler

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
-    //
+    /**
+     * Mostra o dashboard da aplicação
+     *
+     * @return Illuminate\View\View
+     */
+
+    public function index()
+    {
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
+
+
 Auth::routes();
 
-
+Route::get('/', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
 
 
