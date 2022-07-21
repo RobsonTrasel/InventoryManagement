798e7adbe66a17a3eabbab7b0801c2a065ab9911
Robson Fernando Trasel de Souza <95061628+RobsonTrasel@users.noreply.github.com>
2022-07-21 00:50:07 -0300

    Controllers done/rule and request

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PageController extends Controller
 {
-    //
+    /**
+     * Display icons page
+     *
+     * @return \Illuminate\View\View
+     */
+    public function icons()
+    {
+        return view('pages.icons');
+    }
+
+    /**
+     * Display maps page
+     *
+     * @return \Illuminate\View\View
+     */
+    public function maps()
+    {
+        return view('pages.maps');
+    }
+
+    /**
+     * Display tables page
+     *
+     * @return \Illuminate\View\View
+     */
+    public function tables()
+    {
+        return view('pages.tables');
+    }
+
+    /**
+     * Display notifications page
+     *
+     * @return \Illuminate\View\View
+     */
+    public function notifications()
+    {
+        return view('pages.notifications');
+    }
+
+    /**
+     * Display typography page
+     *
+     * @return \Illuminate\View\View
+     */
+    public function typography()
+    {
+        return view('pages.typography');
+    }
+
 }
