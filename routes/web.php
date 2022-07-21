fd3a48f2d76669f775e018642ccde838a9c183fd
Robson Fernando Trasel de Souza <95061628+RobsonTrasel@users.noreply.github.com>
2022-07-21 00:39:32 -0300

    setup controllers/setup routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\MethodController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProductCategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProviderController;
+use App\Http\Controllers\ReceiptController;
+use App\Http\Controllers\SaleController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\TransferController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,5 +23,20 @@ Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
+Route::middleware(['auth'])->group(function () {
+    Route::resources([
+        'users' => UserController::class,
+        'providers' => ProviderController::class,
+        'inventory/products' => ProductController::class,
+        'clients' => ClientController::class,
+        'inventory/categories' => ProductCategoryController::class,
+        'transactions/transfer' => TransferController::class,
+        'methods' => MethodController::class,
+    ]);
+
+    Route::resource('transactions', [TransactionController::class])->except('create', 'show');
+
+
+});
 
 
